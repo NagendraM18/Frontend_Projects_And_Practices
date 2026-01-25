526b591abf56a08d0cef9c2d3c3a4ef903a8ce29
NagendraM18 <nagendramgowda2124@gmail.com>
2026-01-25 10:49:15 +0530

    updated thing  user friendly

--- projects/scripting.js
+++ projects/scripting.js
@@ -1,5 +1,8 @@
 let score=JSON.parse(localStorage.getItem("score")) || {win:0,lose:0,draw:0};
+
             const buttons=document.querySelectorAll("button");//[Rock,Paper,Siscor///reset]
+            const result=document.querySelector(".Js_winning");
+            const scoredisplay=document.querySelector(".Js_score");
             buttons.forEach(button=>{
                 button.addEventListener("click",function()
                     {
@@ -8,34 +11,38 @@ let score=JSON.parse(localStorage.getItem("score")) || {win:0,lose:0,draw:0};
                             score.lose=0;
                             score.draw=0;
                             localStorage.setItem("score",JSON.stringify(score));
+                            result.innerHTML=" You clicked on RESET BUTTON";
+                            scoredisplay.innerText=`score is win ${score.win} lose ${score.lose} draw ${score.draw}`;
                             return 0;
                         }
 
-                        const valueChooseByUser=this.value;
-  
+                    const valueChooseByUser=this.value;
                     const computerValue=["rock","paper","scissor"];
                     const arrayvalue=Math.floor(Math.random()*computerValue.length);
                     const ComputerChoose=computerValue[arrayvalue];
+
+
                     if(valueChooseByUser === ComputerChoose){
-                        score.draw++;
-                         localStorage.setItem("score",JSON.stringify(score));
-                        alert(`computer choose ${ComputerChoose} and it is a Draw
- score is win ${score.win} lose ${score.lose}  draw${score.draw}`);
-                    }else if(
-                      (valueChooseByUser==="rock" && ComputerChoose==="scissor") ||
+                         score.draw++;
+                         result.innerText=`DRAW - you both choose ${valueChooseByUser}`;
+                    }
+                         
+                    else if((valueChooseByUser==="rock" && ComputerChoose==="scissor") ||
                       (valueChooseByUser === "scissor" && ComputerChoose==="paper")||
                       (valueChooseByUser==="paper" && ComputerChoose==="rock") ){
                         score.win++;
-                         localStorage.setItem("score",JSON.stringify(score));
-                             alert(`computer choose ${ComputerChoose} and it is a won 
- score is win ${score.win} lose ${score.lose}  draw${score.draw}`)
+                        result.innerText=`YOU WIN - computer choose ${ComputerChoose} and you choose ${valueChooseByUser}`;
                     }
                      else{
                         score.lose++;
-                         localStorage.setItem("score",JSON.stringify(score));
-                         alert(`computer choose ${ComputerChoose} and it is a  lost
-score is win ${score.win} lose ${score.lose}  draw${score.draw}`);
+                        result.innerText=`YOU LOSE -computer choose ${ComputerChoose} and you choose ${valueChooseByUser}`;
+                     }
+                     
+                     scoredisplay.innerText=`score is win ${score.win} lose ${score.lose}  draw${score.draw}`;
+                     localStorage.setItem("score",JSON.stringify(score));
                     }
-                   }
                 )
             })
+
+            //computer choose ${ComputerChoose} and it is a
+  
